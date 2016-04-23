ef40185fb256289980273780292b3df6f1eda23e
Jake Reynolds <jakealexis@gmail.com>
2016-04-24 00:49:28 +0100

    Changed library for plotting, to allow better management of a continuous time-based domain.

--- app/src/main/java/jake/imperial/drone/fragments/GraphFragment.java
+++ app/src/main/java/jake/imperial/drone/fragments/GraphFragment.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -14,28 +16,47 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.RadioGroup;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.github.mikephil.charting.charts.LineChart;
+import com.androidplot.Plot;
+import com.androidplot.ui.AnchorPosition;
+import com.androidplot.ui.DynamicTableModel;
+import com.androidplot.ui.SizeLayoutType;
+import com.androidplot.ui.SizeMetrics;
+import com.androidplot.ui.XLayoutStyle;
+import com.androidplot.ui.YLayoutStyle;
+import com.androidplot.xy.XYPlot;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
 import jake.imperial.drone.DroneApplication;
 import jake.imperial.drone.R;
 import jake.imperial.drone.utils.Constants;
+import jake.imperial.drone.utils.MqttHandler;
 
+import com.androidplot.xy.*;
+import com.github.mikephil.charting.data.LineData;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GraphFragment extends Fragment {
     private static final String TAG = GraphFragment.class.getName();
@@ -46,8 +67,23 @@ public class GraphFragment extends Fragment {
     private int mInterval = 10000;
     private String domain = "";
     private RequestQueue requestQueue;
-    private LineChart lineChart;
+    private long lastSuccessfulRequest = 0;
 
+    private class PlotUpdater implements Observer {
+        Plot plot;
+
+        public PlotUpdater(Plot plot){
+            this.plot = plot;
+        }
+        @Override
+        public void update(Observable o,Object arg){
+            plot.redraw();
+        }
+    }
+
+    private XYPlot linePlot;
+
+    private Map<String, SimpleXYSeries> data;
 
     public GraphFragment() {
     }
@@ -61,7 +97,9 @@ public class GraphFragment extends Fragment {
         Log.d(TAG, ".onResume() entered()");
         super.onResume();
         app = (DroneApplication) getActivity().getApplication();
-        //app.setCurrentRunningActivity(TAG);
+        app.setCurrentRunningActivity(TAG);
+
+        data = new HashMap<>();
 
         if (broadcastReceiver == null) {
             Log.d(TAG, ".onResume() - Registering GraphBroadcastReceiver");
@@ -80,43 +118,72 @@ public class GraphFragment extends Fragment {
         IntentFilter intentFilter = new IntentFilter(Constants.APP_ID + "." + Constants.ALERT_EVENT);
         getActivity().getApplicationContext().registerReceiver(broadcastReceiver, intentFilter);
 
-
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_graph, container, false);
         mHandler = new Handler();
-        lineChart = (LineChart) rootView.findViewById(R.id.lineChart);
+        //lineChart = (LineChart) rootView.findViewById(R.id.lineChart);
         requestQueue = Volley.newRequestQueue(getContext());
 
-        Button button = (Button) rootView.findViewById(R.id.load_saved_data);
-        button.setOnClickListener(new View.OnClickListener(){
-            boolean selected = false;
+        RadioGroup sensorSource = (RadioGroup) rootView.findViewById(R.id.sensor_source);
+        sensorSource.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v){
-                if(!selected) {
-                    v.setBackgroundColor(getResources().getColor(R.color.control_blue));
-                    startRepeatingTask();
-                } else{
-                    v.setBackgroundColor(getResources().getColor(R.color.common_google_signin_btn_text_dark_disabled));
-                    stopRepeatingTask();
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.load_saved_data:
+                        // TODO Stop MQTT
+                        startLoadingSensorData();
+                        break;
+                    case R.id.mqtt_live_data:
+                        // TODO Start MQTT
+                        stopLoadingSensorData();
+                        break;
                 }
-                selected = !selected;
             }
         });
 
-        //startRepeatingTask();
+
+
+        linePlot = (XYPlot) rootView.findViewById(R.id.linePlot);
+
+
+        linePlot.setDomainStep(XYStepMode.SUBDIVIDE, 10);
+
+        linePlot.setRangeStepMode(XYStepMode.INCREMENT_BY_VAL);
+        linePlot.setRangeStepValue(5);
+
+        linePlot.setDomainValueFormat(new Format(){
+            private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos){
+                Date date = new Date(((Number) obj).longValue());
+                return dateFormat.format(date, toAppendTo, pos);
+            }
+            @Override
+            public Object parseObject(String source, ParsePosition pos){
+                return null;
+            }
+        });
+        linePlot.setRangeValueFormat(new DecimalFormat("###.#"));
+        linePlot.getGraphWidget().setDomainLabelOrientation(-45);
+        linePlot.getLegendWidget().setPadding(10, 10, 10, 10);
+        linePlot.getGraphWidget().setPadding(10,10,10,10);
+        //linePlot.getLegendWidget().setSize(new SizeMetrics(10, SizeLayoutType.ABSOLUTE, 10, SizeLayoutType.ABSOLUTE));
+        //linePlot.getLegendWidget().position(100, XLayoutStyle.ABSOLUTE_FROM_LEFT, 100, YLayoutStyle.ABSOLUTE_FROM_TOP, AnchorPosition.LEFT_TOP);
+
+        //startLoadingSensorData();
         return rootView;
     }
 
     @Override
     public void onDestroyView(){
         super.onDestroyView();
-        stopRepeatingTask();
+        //stopLoadingSensorData();
     }
+
 
     private Runnable updateData = new Runnable() {
         @Override
@@ -124,13 +191,12 @@ public class GraphFragment extends Fragment {
             if(app != null) {
                 domain = app.getDomain();
 
+                // TODO remove this line
                 domain = "192.168.1.76:8080";
 
-                String url = "http://" + domain + "/getSensorData";
+                String url = "http://" + domain + "/getSensorData?fromTime=" + lastSuccessfulRequest;
+                final long lastRequest = System.currentTimeMillis();
                 Log.d(TAG, "Updating sensor data from " + url);
-
-                // null check on context?
-
 
                 JsonArrayRequest request  = new JsonArrayRequest(Request.Method.GET, url, null,
                         new Response.Listener<JSONArray>() {
@@ -138,29 +204,40 @@ public class GraphFragment extends Fragment {
                             public void onResponse(JSONArray response) {
                                 try {
                                     Log.d(TAG, "Received response for sensorData");
-                                    JSONObject sensorData = app.getSensorData();
 
                                     for(int i=0; i<response.length(); i++){
                                         JSONObject item = response.getJSONObject(i);
+                                        String type = item.getString("readingType");
 
-                                        if(sensorData.has(item.getString("readingType"))) {
-                                            JSONArray array = sensorData.getJSONArray(item.getString("readingType"));
-                                            array.put(item);
-                                        }else {
-                                            JSONArray array = new JSONArray();
-                                            array.put(item);
-                                            sensorData.put(item.getString("readingType"), array);
+                                        SimpleXYSeries series = data.get(type);
+                                        if(series != null) {
+                                            series.addLast(item.getLong("time"), item.getDouble("readingValue"));
+                                        } else {
+                                            series = new SimpleXYSeries(type);
+                                            series.addLast(item.getLong("time"), item.getDouble("readingValue"));
+
+                                            LineAndPointFormatter formatter1 = new LineAndPointFormatter(Color.rgb(0, 0, 0), null, null, null);
+                                            formatter1.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
+                                            formatter1.getLinePaint().setStrokeWidth(10);
+
+
+                                            linePlot.addSeries(series,formatter1);
+                                            data.put(type, series);
                                         }
+                                        Log.d(TAG, item.getLong("time") + " " + item.getDouble("readingValue"));
+
                                     }
+                                    linePlot.redraw();
+                                    lastSuccessfulRequest = lastRequest;
 
                                 }catch (JSONException e){
                                     Log.d(TAG, e.toString());
                                 }
                             }
                         }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Log.d(TAG, error.toString());
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d(TAG, error.toString());
                         }
                 });
                 // Add the request to the RequestQueue.
@@ -172,13 +249,22 @@ public class GraphFragment extends Fragment {
 
     };
 
-    private void startRepeatingTask() {
+
+
+    private void startLoadingSensorData() {
         updateData.run();
     }
 
-    private void stopRepeatingTask() {
+    private void stopLoadingSensorData() {
         mHandler.removeCallbacks(updateData);
     }
+
+
+    private void subscribeToSensorData(){
+        MqttHandler mqttHandler = MqttHandler.getInstance(getContext());
+
+    }
+
 
     private void processIntent(Intent intent){
         if(!app.getCurrentRunningActivity().equals(TAG)){return;}
